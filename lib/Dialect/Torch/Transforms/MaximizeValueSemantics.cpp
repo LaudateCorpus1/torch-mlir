e23c98a7c756b4ab72d4d1a0d8ba1ed5edbe3c93
Ramiro Leal-Cavazos <ramiroleal050@gmail.com>
2022-02-16 23:34:45 +0000

    Fix MaximizeValueSemantics handling of `torch.overwrite.tensor` op
    This commit fixes an issue with the way that the
    `maximize-value-semantics` pass was handling the
    `torch.overwrite.tensor` op. Before, the overwrite op would simply get
    deleted and uses of the overwritten tensor would simply get replaced
    with the overwriter tensor. This becomes a problem when one tensor has
    a dynamic shape and the other has a static shape because ops that were
    previously using the overwritten tensor will now get a tensor of a
    different type.
    
    To fix this, this commit adds a static cast to the overwriter tensor
    so that it has the type of the overwritten tensor, and this casted
    tensor is then used to replace the uses of the overwritten tensor.

--- lib/Dialect/Torch/Transforms/MaximizeValueSemantics.cpp
+++ lib/Dialect/Torch/Transforms/MaximizeValueSemantics.cpp
@@ -50,7 +50,26 @@ public:
       if (auto copyToValueTensor = dyn_cast<CopyToValueTensorOp>(user)) {
         rewriter.replaceOp(copyToValueTensor, {currentlyHeldValueTensor});
       } else if (auto overwriteTensor = dyn_cast<OverwriteTensorOp>(user)) {
-        currentlyHeldValueTensor = overwriteTensor.value();
+        Location loc = user->getLoc();
+        Value overwriter = overwriteTensor.value();
+        Value overwritten = overwriteTensor.overwritten();
+        Type overwrittenType = overwritten.getType()
+                                   .dyn_cast<NonValueTensorType>()
+                                   .getWithValueSemantics();
+
+        // Sometimes the overwriter tensor has a different type from the
+        // overwritten tensor. This can happen, for example, if one tensor
+        // has dynamic shape and the other has a static shape. Since the goal
+        // of this pattern is to replace uses of the overwritten tensor with
+        // overwriter tensor, here we cast the overwriter to the type of the
+        // overwritten, to avoid type mismatches later on in the graph.
+        {
+          PatternRewriter::InsertionGuard guard(rewriter);
+          rewriter.setInsertionPointAfter(overwriteTensor);
+          currentlyHeldValueTensor = rewriter.create<TensorStaticInfoCastOp>(
+              loc, overwrittenType, overwriter);
+        }
+
         rewriter.eraseOp(overwriteTensor);
       } else {
         llvm_unreachable("only those ops supported!");
